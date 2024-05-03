978a07edf0c405a54843fa7c5c9e7f1f9df08c00
zulufun <ducphucuuu@gmail.com>
2024-05-03 08:08:55 +0700

    test admin

--- ecourses/courses/admin.py
+++ ecourses/courses/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from django.utils.html import mark_safe
+from .models import Category,Lesson,Course,Tag
 # Register your models here.
+class LessonAdmin(admin.ModelAdmin):
+    class Media:
+        css = {
+            'all':['/static/css/main.css',]
+        }
+        # js = ()
+    list_display = ["id", "subject", "created_date", "active", "course"]
+    search_fields = ["subject", "created_date", "course__subject"]
+    list_filter = ["subject", "course__subject"]
+    readonly_fields = ["avatar"]
+    def avatar (self, lesson):
+        return mark_safe("<img src='/static/{img_url}' alt='{alt}' with = 120px />".format(img_url=lesson.image.name, alt=lesson.subject))
+
+
+
+admin.site.register(Category)
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+
